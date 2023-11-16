a3608161f5a3f1f8b2361d98a180496417436ad4
Maarten Magchiels <maartenmagchiels@gmail.com>
2023-11-16 10:49:13 +0100

    Update Example.js

--- Example.js
+++ Example.js
@@ -1,4 +1,4 @@
-# my_scripts
-repository for on the fly script injection
+///# my_scripts
+///repository for on the fly script injection
 /// ExampleScript.js
 console.log("Hello I'm running via scriptlet injection.");
